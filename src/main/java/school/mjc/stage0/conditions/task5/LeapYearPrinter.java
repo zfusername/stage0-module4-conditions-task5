51eed7589aa86218fe37a87a155bb61521b5c1e6
zfusername <farrukh_ziyaev@epam.com>
2023-07-27 16:26:40 +0500

    fix

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task5;
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
         boolean leap = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
-        String result = leap ? "Leap" : "Not leap";
+        String result = leap ? "leap" : "not leap";
         System.out.println(result);
     }
 }
